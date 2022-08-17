efc2728621908237eb986ca6bfab6900c8e611a0
Emmanuel Ulelu <uleluemmanuel@gmail.com>
2022-08-17 15:33:18 +0100

    Completed functional Version
    Image editor app, pre UI development.

--- Image editor project/script.js
+++ Image editor project/script.js
@@ -5,13 +5,15 @@ filterValue = document.querySelector(".filter-info .value"),
 filterSlider = document.querySelector(".slider input"),
 rotateOptions = document.querySelectorAll(".rotate button"),
 previewImg = document.querySelector(".preview-img img"),
-chooseImgBtn = document.querySelector(".choose-img");
+resetFilterBtn = document.querySelector(".reset-filter"),
+chooseImgBtn = document.querySelector(".choose-img"),
+saveImgBtn = document.querySelector(".save-img");
 
 let brightness = 100, saturation = 100, inversion = 0, grayscale = 0
-let rotate = 0;
+let rotate = 0, flipHorizontal = 1, flipVertical = 1;
 
 const applyFilters = () => {
-    previewImg.style.transform = `rotate(${rotate}deg)`;
+    previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})` ;
     previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
 }
 
@@ -20,6 +22,7 @@ const loadImage = () => {
     if(!file) return; //return if user has not selected file
     previewImg.src = URL.createObjectURL(file); //passing file url as preview bookmark
     previewImg.addEventListener("load", () => {
+        resetFilterBtn.click(); // clicking reset btn, so the filter value resets, if the user selects new a image.
         document.querySelector(".container").classList.remove("disable");
     })
 }
@@ -73,13 +76,51 @@ rotateOptions.forEach(option => {
     option.addEventListener("click", () => { //adding click event listener to all rotate/flip buttons
         if (option.id === "left") {
             rotate -= 90; //if clicked button is left rotate, decrement rotate value by -90
+        } else if (option.id === "right") {
+            rotate += 90; //if clicked button is 1, set this value by +90
+        } else if (option.id === "horizontal") {
+            //if flipHorizontal value is 1, increment rotate value to -1 else set 1
+            flipHorizontal = flipHorizontal === 1 ? -1 : 1;
         } else {
-            rotate += 90;
+            // if flipVertical value is 1, increment rotate value to -1 else set 1
+            flipVertical = flipVertical === 1 ? -1 : 1;
         }
         applyFilters();
     });
 });
 
+
+const resetFilter = () => {
+    //resetting all variables to it's default value
+    brightness = 100, saturation = 100, inversion = 0, grayscale = 0
+    rotate = 0, flipHorizontal = 1, flipVertical = 1;
+    filterOptions [0].click(); //clickng brightness btn, so the brightness selected by default
+    applyFilters ();
+}
+
+const saveImage = () => {
+    const canvas = document.createElement("canvas"); // creating canvas element
+    const ctx = canvas.getContext("2d"); // canvas.getContext returns a drawing context on the canvas
+    canvas.width = previewImg.naturalWidth;
+    canvas.height = previewImg.naturalHeight;
+
+    // applying user selected filters to canvas filter
+    ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+    ctx.translate (canvas.width / 2, canvas.height / 2); // translating canvas from center
+    if(rotate !== 0) { // if rotate value isn't 0, rotate the canvas
+        ctx.rotate(rotate * Math.PI / 180);
+    }
+    ctx.scale(flipHorizontal, flipVertical); // flip canvas, horizontally/vertically
+    ctx.drawImage(previewImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+    
+    const link = document.createElement("a"); // creating <a> element
+    link.download = "image.jpg"; // passing the <a> tag download value to "image.jpg"
+    link.href = canvas.toDataURL(); // passing the <a> tag value to canvas data url
+    link.click(); // clicking <a> tag so the image download 
+}
+
 fileInput.addEventListener("change", loadImage);
 filterSlider.addEventListener("input", updateFilter);
+resetFilterBtn.addEventListener("click", resetFilter);
+saveImgBtn.addEventListener("click", saveImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
